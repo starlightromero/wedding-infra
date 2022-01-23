f9d657a97d20fb916e36809e88a5f815af7e88ff
Starlight Romero <starlightromero@protonmail.com>
2022-01-23 12:34:38 -0800

    Add index to droplet id

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "digitalocean_loadbalancer" "public" {
     protocol = "tcp"
   }
 
-  droplet_ids = digitalocean_kubernetes_cluster.this.node_pool.droplet_id
+  droplet_ids = digitalocean_kubernetes_cluster.this.node_pool[0].droplet_id
 }
 
 resource "digitalocean_kubernetes_cluster" "this" {
